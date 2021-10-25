252f23dce2007b82eedb83478c5e2f41ba5d4fdf
Daniel Reis <danreis.spark@gmail.com>
2021-10-24 21:54:55 -0300

    Base types + board initialization code

---
+++ fsharp/src/DotnetCheckers.Core/Game.fs
@@ -0,0 +1,30 @@
+﻿namespace DotnetCheckers.Core
+
+type Player =
+    | Human of string
+    | AI
+
+type Piece =
+    | Pawn of Player
+    | King of Player
+
+type Cell =
+    | Empty
+    | White of Piece
+    | Black of Piece
+    
+type Board = {
+    CurrentTurn: Player
+    NextTurn: Player
+    Cells: Cell[,]
+}
+
+module Board =
+    let initialize playerBlack playerWhite =
+        let cells: Cell[,] = Array2D.init 8 8 (fun row col -> Empty)
+        
+        {
+            CurrentTurn = playerBlack
+            NextTurn = playerWhite
+            Cells = cells
+        }
