ee71a2708005b9ac35720aee8a7624f121b9c8a6
Daniel Reis <danreis.spark@gmail.com>
2021-10-25 13:25:00 -0300

    Implement movement

--- fsharp/src/DotnetCheckers.Core/Game.fs
+++ fsharp/src/DotnetCheckers.Core/Game.fs
@@ -18,7 +18,18 @@ type Cell =
 type Board =
     { CurrentTurn: Player
       NextTurn: Player
-      Cells: Cell[,] }
+      Cells: Cell [,] }
+
+type Position = { Row: int; Column: int }
+
+type Movement = { To: Position; From: Position }
+
+type CapturedPiece = { Piece: Piece; Position: Position }
+
+type MovementResult =
+    | Failed of string * Board
+    | Succeeded of Board
+    | Captured of CapturedPiece * Board
 
 [<RequireQualifiedAccess>]
 module Piece =
@@ -31,7 +42,7 @@ module Piece =
 module Cell =
     let print blackForeground whiteForeground cell =
         let previousForeground = Console.ForegroundColor
-        
+
         let text =
             match cell with
             | Empty _ -> " "
@@ -60,14 +71,53 @@ module Board =
                [| b; e; b; e; e; e; w; e |]
                [| e; b; e; e; e; w; e; w |]
                [| b; e; b; e; e; e; w; e |]
-               [| e; b; e; e; e; w; e; w |] |] |> array2D
+               [| e; b; e; e; e; w; e; w |] |]
+            |> array2D
 
-        { CurrentTurn = playerBlack
-          NextTurn = playerWhite
+        { CurrentTurn = playerWhite
+          NextTurn = playerBlack
           Cells = cells }
 
+    let getCell position board =
+        board.Cells.[position.Column, position.Row]
+    
+    let move movement board =
+        
+        let swap fromCell toCell board =
+            let piece = board |> getCell fromCell
+            let empty = board |> getCell toCell
+            
+            board.Cells.[fromCell.Column, fromCell.Row] <- empty
+            board.Cells.[toCell.Column, fromCell.Row] <- piece
+        
+        let origin = board |> getCell movement.From
+        let destin = board |> getCell movement.To
+
+        match origin, destin with
+        // no piece to move
+        | Empty, _ -> Failed("There's no piece to move", board)
+        // already a piece on destination cell
+        | _, Black _
+        | _, White _ -> Failed("There's already a piece on the destination", board)
+        | Black piece, _
+        | White piece, _ ->
+            match piece with
+            | Pawn player
+            | King player ->
+                if player.Equals(board.CurrentTurn) |> not then
+                    Failed($"It's not %A{player}'s turn", board)
+                else
+                    board |> swap movement.From movement.To
+                    
+                    Succeeded
+                        { board with
+                              CurrentTurn = board.NextTurn
+                              NextTurn = board.CurrentTurn }
+
     let print board =
-        let background = [| ConsoleColor.Black; ConsoleColor.White |]
+        let background =
+            [| ConsoleColor.Black
+               ConsoleColor.White |]
 
         let mutable index = 0
 
